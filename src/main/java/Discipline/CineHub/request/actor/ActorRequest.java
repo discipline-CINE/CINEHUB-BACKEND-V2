754f7cf41afc7d00eeae04d0f8668f8e2f6fe620
Koul <dongik0122@gmail.com>
2024-02-28 20:17:28 +0900

    actor request
    배우 요청 생성

---
+++ src/main/java/Discipline/CineHub/request/actor/ActorRequest.java
@@ -0,0 +1,29 @@
+package Discipline.CineHub.request.actor;
+
+import Discipline.CineHub.entity.actor.GenderType;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+public class ActorRequest {
+  @NotEmpty
+  private String name;
+  @NotEmpty
+  private GenderType gender;
+  @NotEmpty
+  private Integer birth;
+  @NotEmpty
+  private Double height;
+  @NotEmpty
+  private Double weight;
+  @NotEmpty
+  private String specialty;
+  @NotEmpty
+  private String career;
+  @NotEmpty
+  private String content;
+  private MultipartFile file;
+}
