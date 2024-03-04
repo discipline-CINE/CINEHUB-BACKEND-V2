eca009a488c5b19ddeddabebac04c3882c28a95a
Koul <dongik0122@gmail.com>
2024-03-04 20:38:21 +0900

    get expert types
    모든 전문가 타입을 가져오는 기능 추가

--- src/main/java/Discipline/CineHub/repository/expert/ExpertRepository.java
+++ src/main/java/Discipline/CineHub/repository/expert/ExpertRepository.java
@@ -2,7 +2,13 @@ package Discipline.CineHub.repository.expert;
 
 import Discipline.CineHub.entity.expert.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
-
+  // 맞춤형 쿼리를 작성
+  // DISTINCT은 중복을 제거해준다. 예시로 a,a,b,b,c가 있다면 a,b,c만 나오는 기능이다.
+  @Query("SELECT DISTINCT r.expertType from Expert r")
+  List<String> findDistinctExpertTypes();
 }
--- src/main/java/Discipline/CineHub/service/expert/IExpertService.java
+++ src/main/java/Discipline/CineHub/service/expert/IExpertService.java
@@ -5,7 +5,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface IExpertService {
   Expert addNewExpert(String expertType, String summary, MultipartFile photo) throws IOException, SQLException;
+
+  public List<String> getAllExpertTypes();
 }
