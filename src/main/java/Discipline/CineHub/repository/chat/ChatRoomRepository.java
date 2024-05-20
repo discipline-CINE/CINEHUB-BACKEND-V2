c96e2360fda010b794e6628999972950c55e0178
anjdal713 <anjdal713@naver.com>
2024-05-20 19:33:33 +0900

    chatting list fixed

--- src/main/java/Discipline/CineHub/repository/chat/ChatRoomRepository.java
+++ src/main/java/Discipline/CineHub/repository/chat/ChatRoomRepository.java
@@ -4,10 +4,13 @@ import Discipline.CineHub.domain.ChatRoom;
 import Discipline.CineHub.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByRoomNumber(int roomNumber);
 
     Optional<ChatRoom> findByMemberAndOther(UserEntity member, UserEntity other);
+
+    List<ChatRoom> findByMemberOrOther(UserEntity member, UserEntity other);
 }
--- src/main/java/Discipline/CineHub/service/chat/ChatRoomService.java
+++ src/main/java/Discipline/CineHub/service/chat/ChatRoomService.java
@@ -8,6 +8,7 @@ import Discipline.CineHub.repository.chat.ChatMessageRepository;
 import Discipline.CineHub.repository.chat.ChatRoomRepository;
 import Discipline.CineHub.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +27,11 @@ public class ChatRoomService {
     //전체 채팅방 목록 조회
     @Transactional(readOnly = true)
     public List<ChatRoomResponseDto> findByList() {
-        return chatRoomRepository.findAll().stream()
+        String loginMemberId = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserEntity member = userRepository.findByUsername(loginMemberId)
+                .orElseThrow(() -> new RuntimeException("로그인 유저 정보가 없습니다."));
+
+        return chatRoomRepository.findByMemberOrOther(member, member).stream()
                 .map(chatRoom -> {ChatMessage lastMessage = chatMessageRepository.findTopByRoomNumberOrderByCreatedTimeDesc(chatRoom.getRoomNumber());
                     if (lastMessage!=null) {
                         return new ChatRoomResponseDto(chatRoom, lastMessage.getMsg(), lastMessage.getCreatedTime().format(DateTimeFormatter.ISO_DATE_TIME));
